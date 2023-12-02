1b616c52f08247214c1fa6e751cc12e0f4917067
ab7022 <bayees1@gmail.com>
2023-12-03 03:47:59 +0530

    trying to deploy

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const webpack = require('webpack')
 const serverless = require("serverless-http")
 const http = require("http");
 const router = express.Router()
